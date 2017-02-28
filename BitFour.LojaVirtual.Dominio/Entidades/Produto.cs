dd8ab5ed2c3f8265b4c42664d525acca6984299d
glauberBitfour <grobis_mn@hotmail.com>
2017-02-28 16:38:15 -0300

    commit

--- BitFour.LojaVirtual.Dominio/Entidades/Produto.cs
+++ BitFour.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -9,7 +9,7 @@ namespace BitFour.LojaVirtual.Dominio.Entidades
      public class Produto
     {
         public int produtoId { get; set; }
-        public string NomeProduto { get; set; }
+        public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
